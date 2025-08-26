5bc6b995d5a250f95152b9755578a140b68bb4ef
Maximilian <scientificresearch309@gmail.com>
2025-08-26 21:47:19 +0200

    displaying the randomized article using shuffle function is working well

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import './App.css';
 import germanNoun from './data/germanNouns.json';
 
+// How to produce a random number in Javascript:
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
+// Used like so
+// let arr = [2, 11, 37, 42];
+// shuffle(arr);
+// const randomizedNoun = shuffle(germanNoun);
+shuffle(germanNoun);
+// console.log(arr);
+// console.log(germanNoun);
+
 function App() {
   return (
     <div className="App">
       <h1>German Nouns</h1>
       <h3>There are {germanNoun.length} German nouns in the JSON data!</h3>
+      {/* {randomizedNoun.map((n) => ( */}
       {germanNoun.map((n) => (
         <div className="germanNouns" key={n.singular}>
           <p className="singular">
