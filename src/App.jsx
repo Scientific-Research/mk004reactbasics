dd3c41d59a9dffab9f79b9308d2768e82355b99c
Maximilian <scientificresearch309@gmail.com>
2025-08-26 23:59:55 +0200

    make the app.js empty as psossibel

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
 import './App.css';
 import { GermanNouns } from './components/GermanNouns';
-import _germanNouns from './data/germanNouns.json';
-
-// How to produce a random number in Javascript:
-function shuffle(array) {
-  let currentIndex = array.length;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-  return array;
-}
-
-// Used like so
-// let arr = [2, 11, 37, 42];
-// shuffle(arr);
-// const germanNouns = shuffle(_germanNouns);
-
-//SELECT ONLY THE NOUNS WITH ARTICLE der => using filter method
-const germanNouns = shuffle(_germanNouns).filter((d) => d.article === 'der');
-// shuffle(germanNoun);
-// console.log(arr);
-// console.log(germanNoun);
 
 function App() {
   return (
     <div className="App">
-     
-
-      <GermanNouns GermanNouns={germanNouns} />
+      <GermanNouns />
     </div>
   );
 }
--- src/components/GermanNouns.jsx
+++ src/components/GermanNouns.jsx
@@ -1,9 +1,41 @@
-export const GermanNouns = ({ GermanNouns }) => {
+import _germanNouns from '../data/germanNouns.json';
+
+// How to produce a random number in Javascript:
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+// Used like so
+// let arr = [2, 11, 37, 42];
+// shuffle(arr);
+// const germanNouns = shuffle(_germanNouns);
+
+//SELECT ONLY THE NOUNS WITH ARTICLE der => using filter method
+const germanNouns = shuffle(_germanNouns).filter((d) => d.article === 'der');
+// shuffle(germanNoun);
+// console.log(arr);
+// console.log(germanNoun);
+
+export const GermanNouns = () => {
   return (
     <>
       <h1>German Nouns</h1>
-      <h3>There are {GermanNouns.length} German nouns in the JSON data!</h3>
-      {GermanNouns.map((n) => (
+      <h3>There are {germanNouns.length} German nouns in the JSON data!</h3>
+      {germanNouns.map((n) => (
         <div className="germanNouns" key={n.singular}>
           <img src="/images/cardback2.png" alt="" />
           <div>
